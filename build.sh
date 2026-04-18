02536be06478c17912c0525307f0a17c51c4c4b6
MoeClub <32381827+MoeClub@users.noreply.github.com>
2026-04-18 19:49:24 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,19 +6,28 @@ dnsVer="${2:-2.92}"
 dockerBase="alpine:3.20"
 dockerName="ocserv_build"
 
+case `uname -m` in aarch64|arm64) arch="arm64";; x86_64|amd64) arch="amd64";; *) arch="";; esac
+[ -n "$arch" ] || exit 1
+
 docker rm -f "${dockerName}" >/dev/null 2>&1;
 docker run --name "${dockerName}" -id -v /mnt:/mnt "${dockerBase}"
 docker exec "${dockerName}" /bin/sh /mnt/commit.sh "${ocVer}" "${dnsVer}"
 docker commit --change 'CMD ["/bin/sh", "/run.sh"]' "${dockerName}" "ocserv:${ocVer}"
-docker tag "ocserv:${ocVer}" ocserv:latest
+docker tag "ocserv:${ocVer}" "${arch}:latest"
 docker rm -f "${dockerName}" >/dev/null 2>&1;
 
 userName="$(docker info 2>/dev/null |grep 'Username:' |cut -d':' -f2 |sed 's/[[:space:]]//g')"
 [ -n "$userName" ] || exit 0
-docker tag ocserv:latest "${userName}/ocserv:latest"
-docker push "${userName}/ocserv:latest"
-docker tag ocserv:latest "${userName}/ocserv:${ocVer}"
-docker push "${userName}/ocserv:${ocVer}"
+docker tag "${arch}:latest" "${userName}/${arch}:latest"
+docker push "${userName}/${arch}:latest"
+docker tag "${arch}:latest" "${userName}/${arch}:${ocVer}"
+docker push "${userName}/${arch}:${ocVer}"
+
+
+# docker manifest create ocserv/ocserv:latest --amend ocserv/amd64:latest --amend ocserv/arm64:latest
+# docker manifest push ocserv/ocserv:latest
+# docker manifest create ocserv/ocserv:1.4.1 --amend ocserv/amd64:1.4.1 --amend ocserv/arm64:1.4.1
+# docker manifest push ocserv/ocserv:1.4.1
 
 # docker pull ocserv/ocserv:latest
 # docker run --privileged --rm -it -p 443:443 ocserv/ocserv
