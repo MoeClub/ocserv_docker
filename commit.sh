162268660e1051da6a7742366de0eaedd9c16368
MoeClub <32381827+MoeClub@users.noreply.github.com>
2026-04-18 22:42:02 +0800

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -9,6 +9,7 @@ mkdir -p /etc/dnsmasq.d /etc/ocserv/group
 [ -f /mnt/Default ] && cp -rf /mnt/Default /etc/ocserv/group
 [ -f /mnt/NoRoute ] && cp -rf /mnt/NoRoute /etc/ocserv/group
 [ -f /mnt/ocserv.conf ] && cp -rf /mnt/ocserv.conf /etc/ocserv
+[ -f /mnt/p12.sh ] && cp -rf /mnt/p12.sh /etc/ocserv
 [ -f /mnt/dnsmasq.conf ] && cp -rf /mnt/dnsmasq.conf /etc/dnsmasq.conf
 [ -f /mnt/run.sh ] && cp -rf /mnt/run.sh /run.sh
 chmod -R 777 /etc/dnsmasq.d /etc/dnsmasq.conf /etc/ocserv /run.sh
