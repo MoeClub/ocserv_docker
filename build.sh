504d72e9ccb8739fc1a6af9d761e6cddac0724eb
MoeClub <32381827+MoeClub@users.noreply.github.com>
2026-04-18 15:33:47 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@ set -e
 
 ocVer="${1:-1.4.1}"
 dnsVer="${2:-2.92}"
+dockerBase="alpine:3.20"
 dockerName="ocserv_build"
 
 docker rm -f "${dockerName}" >/dev/null 2>&1;
-docker run --name "${dockerName}" -id -v /mnt:/mnt alpine:3.20
+docker run --name "${dockerName}" -id -v /mnt:/mnt "${dockerBase}"
 docker exec "${dockerName}" /bin/sh /mnt/commit.sh "${ocVer}" "${dnsVer}"
 docker commit --change 'CMD ["/bin/sh", "/run.sh"]' "${dockerName}" "ocserv:${ocVer}"
 docker tag "ocserv:${ocVer}" ocserv:latest
