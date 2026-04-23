ce10c58ec0fc899cdff85b674ac42d66d06bc453
MoeClub <32381827+MoeClub@users.noreply.github.com>
2026-04-23 21:26:28 +0800

    Update buildx.sh

--- buildx.sh
+++ buildx.sh
@@ -1,33 +1,35 @@
 #!/bin/sh
 set -e
 
-ocVer="${1:-1.4.2}"
-dnsVer="${2:-2.92}"
+oneVer="${1:-1.4.1}"
+twoVer="${2:-2.92}"
 withLatest="${3:-0}"
+dockerProject="ocserv"
 dockerBase="alpine:3.20"
-dockerName="ocserv_buildx"
+dockerName="${dockerProject}_buildx"
+
 
 manifest=""
 for arch in "amd64" "arm64"; do
   docker run --privileged --rm tonistiigi/binfmt --install "${arch}"
   docker rm -f "${dockerName}" >/dev/null 2>&1 || true
   docker run --platform "linux/${arch}" --name "${dockerName}" -id -v /mnt:/mnt "${dockerBase}"
-  docker exec "${dockerName}" /bin/sh /mnt/commit.sh "${ocVer}" "${dnsVer}"
-  docker commit --change 'CMD ["/bin/sh", "/run.sh"]' "${dockerName}" "${arch}:${ocVer}"
+  docker exec "${dockerName}" /bin/sh /mnt/commit.sh "${oneVer}" "${twoVer}"
+  docker commit --change 'CMD ["/bin/sh", "/run.sh"]' "${dockerName}" "${dockerProject}_${arch}:${oneVer}"
   docker rm -f "${dockerName}" >/dev/null 2>&1 || true
   userName="$(docker info 2>/dev/null |grep 'Username:' |cut -d':' -f2 |sed 's/[[:space:]]//g')"
   [ -n "$userName" ] || continue
   [ "${withLatest}" = "1" ] && {
-    docker tag "${arch}:${ocVer}" "${userName}/${arch}:latest"
-    docker push "${userName}/${arch}:latest"
+    docker tag "${dockerProject}_${arch}:${oneVer}" "${userName}/${dockerProject}_${arch}:latest"
+    docker push "${userName}/${dockerProject}_${arch}:latest"
   }
-  docker tag "${arch}:${ocVer}" "${userName}/${arch}:${ocVer}"
-  docker push "${userName}/${arch}:${ocVer}"
-  [ $? -eq 0 ] && manifest=`echo "--amend \"${userName}/${arch}:${ocVer}\" ${manifest}" |sed 's/\ \+$//'`
+  docker tag "${dockerProject}_${arch}:${oneVer}" "${userName}/${dockerProject}_${arch}:${oneVer}"
+  docker push "${userName}/${dockerProject}_${arch}:${oneVer}"
+  [ $? -eq 0 ] && manifest=`echo "--amend \"${userName}/${dockerProject}_${arch}:${oneVer}\" ${manifest}" |sed 's/\ \+$//'`
 done
 
-[ -n "$manifest" ] && eval `echo "docker manifest create \"${userName}/ocserv:${ocVer}\" $manifest"` && docker manifest push -p "${userName}/ocserv:${ocVer}"
-[ -n "$manifest" ] && [ "${withLatest}" = "1" ] && eval `echo "docker manifest create \"${userName}/ocserv:latest\" $manifest"` && docker manifest push -p "${userName}/ocserv:latest"
+[ -n "$userName" ] && [ -n "$manifest" ] && eval `echo "docker manifest create \"${userName}/${dockerProject}:${oneVer}\" $manifest"` && docker manifest push -p "${userName}/${dockerProject}:${oneVer}"
+[ -n "$userName" ] && [ -n "$manifest" ] && [ "${withLatest}" = "1" ] && eval `echo "docker manifest create \"${userName}/${dockerProject}:latest\" $manifest"` && docker manifest push -p "${userName}/${dockerProject}:latest"
 
 
 # docker pull ocserv/ocserv:latest
